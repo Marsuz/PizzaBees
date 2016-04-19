2925f092ba132c6ec022343f0aed19a0c865e8a1
Marsuz <marcin.zajda@gmail.com>
2016-04-19 20:16:39 +0200

    gson dependency added

---
+++ src/main/java/JsonSample.java
@@ -0,0 +1,15 @@
+import model.Restaurant;
+
+import java.util.List;
+
+/**
+ * Created by Marcin on 2016-04-19.
+ */
+public class JsonSample {
+
+    public List<Restaurant> restaurants = null;
+
+
+
+
+}
