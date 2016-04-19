a3adfa65a29269ed0bfd87748862b848a4c65e7a
Marsuz <marcin.zajda@gmail.com>
2016-04-19 20:48:24 +0200

    input format in json

---
+++ src/main/java/AppInput.java
@@ -0,0 +1,35 @@
+import com.google.gson.Gson;
+import model.Order;
+import model.Restaurant;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppInput {
+
+    private List<Restaurant> restaurants = null;
+    private int P;
+    private List<Order> orders = null;
+
+    public AppInput() {
+        restaurants = new ArrayList<Restaurant>();
+        restaurants.add(new Restaurant(10, 10, 5));
+        restaurants.add(new Restaurant(20, 15, 10));
+        P = 10;
+        orders = new ArrayList<Order>();
+        orders.add(new Order(20, 20, 5));
+    }
+
+    public static void main(String[] args) {
+        AppInput input = new AppInput();
+        System.out.println(convertToJson(input));
+    }
+
+    private static String convertToJson(AppInput input){
+        Gson gson = new Gson();
+        return gson.toJson(input);
+    }
+
+
+
+}
--- src/main/java/model/Restaurant.java
+++ src/main/java/model/Restaurant.java
@@ -7,13 +7,16 @@ import java.util.*;
  */
 public class Restaurant extends Location {
 
+    private int courierNumber;
     private Set<Courier> couriers = null;
     private List<Delivery> deliveries = null;
     public static int P;
 
-    public Restaurant(int x, int y) {
+    public Restaurant(int x, int y, int courierNumber) {
         super(x, y);
+        this.courierNumber = courierNumber;
         couriers = new HashSet<Courier>();
+        //for(int i = 0; i < courierNumber; i++) couriers.add(new Courier());
         deliveries =  new ArrayList<Delivery>();
     }
 
