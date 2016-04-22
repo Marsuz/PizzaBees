778a2f6efd107c04251b50ad23f5e0c0259be9ef
Jakub Kudzia <jakub.kudzia@outlook.com>
2016-04-23 01:18:49 +0200

    test of getInitialSetting passes

---
+++ src/main/test/model/solver/setting/RandomSettingFactoryTest.java
@@ -0,0 +1,61 @@
+package model.solver.setting;
+
+import model.Delivery;
+import model.Order;
+import model.Restaurant;
+import org.junit.Before;
+import org.junit.Test;
+import solver.setting.RandomSettingFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Jakub Kudzia on 4/23/16.
+ */
+public class RandomSettingFactoryTest {
+
+    private RandomSettingFactory randomSettingFactory;
+    private List<Restaurant> restaurants;
+    private List<Order> orders;
+    private static final int RESTAURANTS_NUM = 10;
+    private static final int ORDERS_NUM = 173;
+
+    @Before
+    public void setup() {
+
+        restaurants = new ArrayList<>(RESTAURANTS_NUM);
+        for (int i = 0; i < RESTAURANTS_NUM; i++) {
+            restaurants.add(new Restaurant(i, i, i));
+        }
+
+        orders = new ArrayList<>(ORDERS_NUM);
+        for (int i = 0; i < ORDERS_NUM; i++) {
+            orders.add(new Order(i, i, i));
+        }
+
+        randomSettingFactory = new RandomSettingFactory(restaurants);
+    }
+
+    @Test
+    public void testGetInitialSetting(){
+        List<Restaurant> restaurantsWithSettings = randomSettingFactory.getInitialSetting(orders);
+        int setOrdersNumber = 0;
+
+        for(Restaurant r: restaurantsWithSettings)
+            setOrdersNumber += getOrdersNumber(r.getDeliveries());
+        assertEquals(ORDERS_NUM, setOrdersNumber);
+    }
+
+
+    private int getOrdersNumber(List<Delivery> deliveries){
+        int sum = 0;
+        for(Delivery d: deliveries)
+            sum += d.getOrders().size();
+
+        return sum;
+    }
+
+}
