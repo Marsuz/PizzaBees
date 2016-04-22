316a98c955afdb4de39e4c45ee20c1cef54785fe
Jakub Kudzia <jakub.kudzia@outlook.com>
2016-04-22 23:45:07 +0200

    change SettingFactory to abstract class

--- src/main/java/solver/setting/SettingFactory.java
+++ src/main/java/solver/setting/SettingFactory.java
@@ -1,5 +1,6 @@
 package solver.setting;
 
+import model.Order;
 import model.Restaurant;
 
 import java.util.List;
@@ -7,9 +8,14 @@ import java.util.List;
 /**
  * Created by Admin on 2016-04-22.
  */
-public interface SettingFactory {
+public abstract class SettingFactory {
 
-    public List<Restaurant> getInitialSetting();
+    List<Restaurant> restaurants;
 
-    public List<Restaurant> getNeighbourSetting(List<Restaurant> restaurants);
+    public SettingFactory(List<Restaurant> restaurants) {
+        this.restaurants = restaurants;
+    }
+
+    public abstract List<Restaurant> getInitialSetting(List<Order> orders);
+    public abstract List<Restaurant> getNeighbourSetting(List<Restaurant> restaurants);
 }
