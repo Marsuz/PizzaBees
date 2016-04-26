c550e8e41a8890b4d57aed4f7a563089cf657a59
Stanisław Chmiela <sjchmiela@gmail.com>
2016-04-26 09:35:57 +0200

    Add initializer for Dashboard

---
+++ src/main/java/gui/GUIApp.java
@@ -0,0 +1,12 @@
+package gui;
+
+import javax.swing.*;
+
+/**
+ * Created by sjchmiela on 26.04.2016.
+ */
+public class GUIApp {
+    public static void main(String[] args) {
+        Dashboard window = new Dashboard();
+    }
+}
