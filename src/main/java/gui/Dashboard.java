d03ec52aab026f494b98edd75bb6d8a7503dcd65
Stanisław Chmiela <sjchmiela@gmail.com>
2016-04-26 09:35:47 +0200

    Create initial Dashboard layout with connected JFrame class

---
+++ src/main/java/gui/Dashboard.java
@@ -0,0 +1,25 @@
+package gui;
+
+import javax.swing.*;
+
+/**
+ * Created by sjchmiela on 26.04.2016.
+ */
+public class Dashboard extends JFrame {
+    private JPanel rootPanel;
+    private JTextArea inputTextArea;
+    private JTextField fileTextField;
+    private JButton chooseFileButton;
+    private JButton runButton;
+    private JProgressBar progressBar;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+
+    public Dashboard() {
+        super("PizzaBees");
+        setContentPane(rootPanel);
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
