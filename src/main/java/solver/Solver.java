1654566caa1a1d57e63579de3c2c7370f328a435
wgrabis <wgrabis@gmail.com>
2016-05-12 19:26:14 +0200

    tosolve added

--- src/main/java/solver/Solver.java
+++ src/main/java/solver/Solver.java
@@ -1,9 +1,56 @@
 package solver;
 
+import model.Order;
+import model.Restaurant;
+import model.Setting;
+import solver.setting.AbstractSettingFactory;
+import solver.setting.SettingFactory;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
 /**
  * Created by Marcin on 2016-04-19.
  */
 public class Solver {
 
-    // TODO: 2016-04-19  
+    List<Restaurant> beginRestaurants;
+    List<Order> beginOrders;
+
+    public Solver(List<Restaurant> restaurants, List<Order> orders){
+        beginOrders = orders;
+        beginRestaurants = restaurants;
+    }
+
+    public void solve(
+            SettingFactory factory,
+            double distanceWage,
+            double timeWage,
+            int scoutsQuantity,
+            int selectedSites,
+            int bestSites,
+            int eliteQuantity,
+            int normalQuantity,
+            int iterations
+    ){
+        List<Setting> scoutsSettings = new ArrayList<>();
+        IntStream.range(0, scoutsQuantity).forEach(
+                n -> scoutsSettings.add(n, factory.getInitialSetting(beginOrders))
+        );
+
+        while(iterations-- != 0) { // TODO: second stop condition
+            Collections.sort(scoutsSettings,
+                    (s1, s2) -> Double.compare(
+                            s1.getFitness(distanceWage, timeWage),
+                            s2.getFitness(distanceWage, timeWage)
+                    )
+            );
+
+
+
+        }
+
+    }
 }
