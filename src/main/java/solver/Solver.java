6687623ff2a0499557b5dacbfcf7cea452d5b04c
wgrabis <wgrabis@gmail.com>
2016-05-12 21:59:04 +0200

    Solver implementation

--- src/main/java/solver/Solver.java
+++ src/main/java/solver/Solver.java
@@ -14,7 +14,6 @@ import java.util.stream.IntStream;
  * Created by Marcin on 2016-04-19.
  */
 public class Solver {
-
     List<Restaurant> beginRestaurants;
     List<Order> beginOrders;
 
@@ -32,23 +31,64 @@ public class Solver {
             int bestSites,
             int eliteQuantity,
             int normalQuantity,
-            int iterations
+            int iterations,
+            int movesNum
     ){
-        List<Setting> scoutsSettings = new ArrayList<>();
+        List<Setting> iterationSettings = new ArrayList<>();
         IntStream.range(0, scoutsQuantity).forEach(
-                n -> scoutsSettings.add(n, factory.getInitialSetting())
+                n -> iterationSettings.add(n, factory.getInitialSetting())
         );
 
         while(iterations-- != 0) { // TODO: second stop condition
-            Collections.sort(scoutsSettings,
+            List<Setting> newSetting = new ArrayList<>();
+
+            Collections.sort(iterationSettings,
                     (s1, s2) -> Double.compare(
-                            s1.getFitness(distanceWage, timeWage),
-                            s2.getFitness(distanceWage, timeWage)
+                            s2.getFitness(distanceWage, timeWage),
+                            s1.getFitness(distanceWage, timeWage)
                     )
             );
 
+            IntStream.range(0, bestSites).forEach(
+                    n -> {
+                        List<Setting> tempSettings = new ArrayList<>();
+                        IntStream.range(0, eliteQuantity).forEach(
+                                m -> tempSettings.add(iterationSettings.get(n).getNeighbour(movesNum))
+                        );
+                        Collections.sort(tempSettings,
+                                (s1, s2) -> Double.compare(
+                                        s2.getFitness(distanceWage, timeWage),
+                                        s1.getFitness(distanceWage, timeWage)
+                                )
+                        );
+                        newSetting.add(tempSettings.get(0));
+                    }
 
+            );
+            IntStream.range(bestSites, selectedSites).forEach(
+                    n -> {
+                        List<Setting> tempSettings = new ArrayList<>();
+                        IntStream.range(0, normalQuantity).forEach(
+                                m -> tempSettings.add(iterationSettings.get(n).getNeighbour(movesNum))
+                        );
+                        Collections.sort(tempSettings,
+                                (s1, s2) -> Double.compare(
+                                        s2.getFitness(distanceWage, timeWage),
+                                        s1.getFitness(distanceWage, timeWage)
+                                )
+                        );
+                        newSetting.add(tempSettings.get(0));
+                    }
+            );
 
+            int scoutsLeft = scoutsQuantity - bestSites * eliteQuantity - (selectedSites - bestSites) * normalQuantity;
+
+            IntStream.range(0, scoutsLeft).forEach(
+                    n -> newSetting.add(n, factory.getInitialSetting())
+            );
+
+            iterationSettings.clear();
+            iterationSettings.addAll(newSetting);
         }
 
     }
