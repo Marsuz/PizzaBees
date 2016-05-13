89cee0960f2b863c95ebbc3659e28e4bbf9779ac
Marcin Majewski <majewskimarcin@outlook.com>
2016-05-13 16:53:40 +0200

    * Creating gson class for deserialization SavedStateDeserializer * Sample json created (added velocity arugment) * Preconditions added

--- src/main/java/app/AppInput.java
+++ src/main/java/app/AppInput.java
@@ -10,39 +10,45 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 
 public class AppInput {
-    private static final Logger logger = Logger.getLogger(AppInput.class);
-    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    private SavedState savedState;
+	private static final Logger logger = Logger.getLogger(AppInput.class);
+	private SavedState savedState;
 
-    AppInput(String pathToInputFile) {
-        File inputFile = new File(pathToInputFile);
-        if (!inputFile.exists()) {
-            logger.error(String.format("File with path: %s not exist", pathToInputFile));
-            System.exit(1);
-        }
-        if (!inputFile.isFile()) {
-            logger.error(String.format("File with path: %s is not a file", pathToInputFile));
-            System.exit(1);
-        }
-        try {
-            String jsonContentOfFile = FileUtils.readFileToString(inputFile, Charset.forName("UTF-8"));
-            savedState = gson.fromJson(jsonContentOfFile, SavedState.class);
-            logger.debug(String.format("Resolved app state:\n %s", gson.toJson(savedState)));
-        } catch (IOException e) {
-            logger.error(String.format("Error while reading from file: %s", inputFile.getAbsolutePath()), e);
-        }
-    }
+	public AppInput(String pathToInputFile) {
+		GsonBuilder gsonBuilder = new GsonBuilder();
+		gsonBuilder.setPrettyPrinting().registerTypeAdapter(SavedState.class,new SavedStateDeserializer());
+		Gson gson = gsonBuilder.create();
 
-    public SavedState getSavedState(){
-        return savedState;
-    }
+		File inputFile = new File(pathToInputFile);
+		if (!inputFile.exists()) {
+			logger.error(String.format("File with path: %s not exist", pathToInputFile));
+			System.exit(1);
+		}
+		if (!inputFile.isFile()) {
+			logger.error(String.format("File with path: %s is not a file", pathToInputFile));
+			System.exit(1);
+		}
+		try {
+			String jsonContentOfFile = FileUtils.readFileToString(inputFile, Charset.forName("UTF-8"));
+			savedState = gson.fromJson(jsonContentOfFile, SavedState.class);
+			logger.debug(String.format("Resolved app state:\n %s", gson.toJson(savedState)));
+		} catch (IOException e) {
+			logger.error(String.format("Error while reading from file: %s", inputFile.getAbsolutePath()), e);
+		}
+	}
 
-/*
-    private static String convertToJson(AppInput input) {
-        Gson gson = new Gson();
-        return gson.toJson(input);
-    }
-*/
+	public SavedState getSavedState() {
+		return savedState;
+	}
 
+	/*
+		private static String convertToJson(AppInput input) {
+			Gson gson = new Gson();
+			return gson.toJson(input);
+		}
+	*/
+	public static void main(String[] args) {
+		AppInput appInput = new AppInput("/home/marcin/Development/Studies/PizzaBees/src/main/resources/inputSample.json");
+		System.out.println();
+	}
 
 }
---
+++ src/main/java/app/SavedStateDeserializer.java
@@ -0,0 +1,74 @@
+package app;
+
+import com.google.common.base.Preconditions;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import model.Courier;
+import model.Order;
+import model.Restaurant;
+import org.apache.log4j.Logger;
+
+import java.lang.reflect.Type;
+import java.util.LinkedList;
+import java.util.List;
+
+class SavedStateDeserializer implements JsonDeserializer<SavedState> {
+
+	private static final Logger logger = Logger.getLogger(SavedStateDeserializer.class);
+
+	@Override
+	public SavedState deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonContext)
+			throws JsonParseException {
+		JsonObject mainJsonObject = jsonElement.getAsJsonObject();
+
+		List<Restaurant> restaurantList = new LinkedList<>();
+		JsonArray restaurants = mainJsonObject.getAsJsonArray("restaurants");
+		Preconditions.checkNotNull(restaurants,"No restaurants present in json!");
+		for (JsonElement restaurant : restaurants) {
+			restaurantList.add(processOneRestaurant(restaurant));
+		}
+
+		final JsonPrimitive pJsonPrimitive = mainJsonObject.getAsJsonPrimitive("P");
+		Preconditions.checkNotNull(pJsonPrimitive,"No P parameter present in json!");
+		int p = pJsonPrimitive.getAsInt();
+
+		List<Order> orderList = new LinkedList<>();
+		JsonArray orders = mainJsonObject.getAsJsonArray("orders");
+		Preconditions.checkNotNull(orders,"No orders present in json!");
+		for (JsonElement order : orders) {
+			orderList.add(processOneOrder(order));
+		}
+
+		final JsonPrimitive velocityJson = mainJsonObject.getAsJsonPrimitive("velocity");
+		Preconditions.checkNotNull(velocityJson, "No velocity present in json!");
+		int velocity = velocityJson.getAsInt();
+		logger.info(String.format("Setting velocity for class %s as %d",Courier.class,velocity));
+		Courier.velocity = velocity;
+
+		return new SavedState(restaurantList,p,orderList);
+	}
+
+	private Order processOneOrder(JsonElement jsonOrderElement) {
+		JsonObject orderJsonObject = jsonOrderElement.getAsJsonObject();
+		int quantity = orderJsonObject.getAsJsonPrimitive("quantity").getAsInt();
+
+		JsonObject destinationObject = orderJsonObject.getAsJsonObject("destination");
+		final int x = destinationObject.getAsJsonPrimitive("x").getAsInt();
+		final int y = destinationObject.getAsJsonPrimitive("y").getAsInt();
+
+		return new Order(x,y,quantity);
+	}
+
+	private Restaurant processOneRestaurant(JsonElement restaurant) {
+		JsonObject restaurantJsonObject = restaurant.getAsJsonObject();
+		int numberOfCouriers = restaurantJsonObject.getAsJsonPrimitive("courierNumber").getAsInt();
+		int x = restaurantJsonObject.getAsJsonPrimitive("x").getAsInt();
+		int y = restaurantJsonObject.getAsJsonPrimitive("y").getAsInt();
+		return new Restaurant(x, y, numberOfCouriers);
+	}
+}
