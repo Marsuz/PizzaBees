f022b9806bf29d59e1f8a75eddb962292a4fae42
Marcin Majewski <majewskimarcin@outlook.com>
2016-05-13 18:36:29 +0200

    * Catching IOException

--- src/main/java/app/ResultsDumper.java
+++ src/main/java/app/ResultsDumper.java
@@ -13,17 +13,21 @@ import java.io.IOException;
 class ResultsDumper {
 	private final static Logger logger = Logger.getLogger(ResultsDumper.class);
 	
-	static void dumpResultsToFile(String outputPath, Setting result) throws IOException {
-		Preconditions.checkNotNull(outputPath,"Output path must nor be null!");
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		String outputResultJson = gson.toJson(result);
-		logger.debug(String.format("Result: \n %s", outputResultJson));
-		File file = new File(outputPath);
-		boolean ifCreated = file.createNewFile();
-		if(!ifCreated){
-			logger.warn(String.format("File was not created for path :%s",outputPath));
+	static void dumpResultsToFile(String outputPath, Setting result) {
+		try {
+			Preconditions.checkNotNull(outputPath,"Output path must nor be null!");
+			Gson gson = new GsonBuilder().setPrettyPrinting().create();
+			String outputResultJson = gson.toJson(result);
+			logger.debug(String.format("Result: \n %s", outputResultJson));
+			File file = new File(outputPath);
+			boolean ifCreated = file.createNewFile();
+			if(!ifCreated){
+				logger.warn(String.format("File was not created for path :%s",outputPath));
+			}
+			logger.info(String.format("Writing output data to file: %s",file.getAbsolutePath()));
+			FileUtils.writeStringToFile(file,outputResultJson,"UTF-8");
+		} catch (IOException e) {
+			logger.error(String.format("Error while saving to file %s",outputPath),e);
 		}
-		logger.info(String.format("Writing output data to file: %s",file.getAbsolutePath()));
-		FileUtils.writeStringToFile(file,outputResultJson,"UTF-8");
 	}
 }
